14421d1c759b0263380ecbede5628c003ffee898
JessicaOI <70552512+JessicaOI@users.noreply.github.com>
2023-05-20 22:14:57 -0600

    terminado analisis de Product clasification

--- Product_Clasification/main.py
+++ Product_Clasification/main.py
@@ -1,92 +1,87 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from transformers import BertTokenizer, TFBertForSequenceClassification
-from tensorflow.keras.optimizers import Adam
-from nltk.corpus import stopwords
-import re
-import seaborn as sns
 import matplotlib.pyplot as plt
-
-# Paso 1: Carga y limpieza de datos
-df = pd.read_csv('Participants_Data/Sample Submission.csv')
-
-# Eliminar valores nulos
-df.dropna(inplace=True)
-
-# Eliminar duplicados
-df.drop_duplicates(inplace=True)
-
-# Eliminación de caracteres especiales, números y espacios extra
-df['comentario_limpio'] = df['comentario'].apply(lambda x: re.sub(r'[^\w\s]', '', x.lower()))
-df['comentario_limpio'] = df['comentario_limpio'].apply(lambda x: re.sub(r'\d+', '', x))
-df['comentario_limpio'] = df['comentario_limpio'].apply(lambda x: re.sub(r'\s+', ' ', x))
-
-# Lematización/Stemming y eliminación de palabras vacías - Este paso puede variar dependiendo del idioma y la biblioteca que se utilice
-
-# Paso 2: Análisis Exploratorio de Datos
-# Aquí puedes hacer gráficos y otras formas de análisis. Este paso dependerá de tus datos específicos.
-
-# Paso 3: Preprocesamiento para BERT
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)  # O el modelo BERT que elijas
-encoded_data = tokenizer.batch_encode_plus(
-    df['comentario_limpio'].tolist(),
-    add_special_tokens=True,
-    return_attention_mask=True,
-    pad_to_max_length=True,
-    max_length=256,
-    return_tensors='tf'
-)
-
-# Dividir en conjuntos de entrenamiento y prueba
-input_ids = encoded_data['input_ids']
-attention_masks = encoded_data['attention_mask']
-labels = df['etiqueta'].values  # Asegúrate de que 'etiqueta' es el nombre correcto de tu columna de etiquetas
-train_inputs, test_inputs, train_labels, test_labels, train_masks, test_masks = train_test_split(
-    input_ids,
-    labels,
-    attention_masks,
-    random_state=42,
-    test_size=0.1
-)
-
-# Paso 4: Entrenar el modelo
-model = TFBertForSequenceClassification.from_pretrained(
-    'bert-base-uncased',  # O el modelo BERT que elijas
-    num_labels=2  # Cambia esto si tienes más de dos etiquetas
-)
-optimizer = Adam(learning_rate=5e-5)
-model.compile(optimizer=optimizer, loss=model.compute_loss, metrics=['accuracy'])
-history = model.fit(
-    [train_inputs, train_masks],
-    train_labels,
-    batch_size=16,  # Ajusta esto según el tamaño de tu GPU
-    epochs=4  # Ajusta esto según tus necesidades
-)
-
-# Paso 5: Evaluar el modelo
-model_eval = model.evaluate([test_inputs, test_masks], test_labels, batch_size=16)
-
-# Mostrar las métricas de evaluación
-print("Loss del modelo: ", model_eval[0])
-print("Precisión del modelo: ", model_eval[1])
-
-# También puedes hacer predicciones y ver cómo se ven para algunos ejemplos.
-predictions = model.predict([test_inputs, test_masks])
-
-# Paso 6: Interpretación de los resultados
-# Convertir las predicciones a etiquetas de clase
-predictions_labels = np.argmax(predictions.logits, axis=-1)
-
-# Mostrar las predicciones junto a las etiquetas reales en un DataFrame
-results_df = pd.DataFrame({'Predicciones': predictions_labels, 'Etiquetas Reales': test_labels})
-
-# Mostrar las primeras filas del DataFrame
-print(results_df.head())
-
-# También puedes usar técnicas como la matriz de confusión para visualizar el rendimiento del modelo
-from sklearn.metrics import confusion_matrix
 import seaborn as sns
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+from collections import Counter
 
-cm = confusion_matrix(test_labels, predictions_labels)
-sns.heatmap(cm, annot=True, fmt='d')
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Cargar el csv
+df = pd.read_csv('Train.csv')
+
+# Mapeo de sentimientos
+sentiment_mapping = {
+    0: 'Cannot Say',
+    1: 'Negative',
+    2: 'Positive',
+    3: 'No Sentiment'
+}
+
+# Reemplazar los números por los nombres en la columna de Sentimientos
+df['Sentiment'] = df['Sentiment'].replace(sentiment_mapping)
+
+# Imprimir las primeras filas para revisar los datos
+print(df.head())
+
+# Imprimir la información básica del dataframe
+print(df.info())
+
+# Descripción estadística del dataframe
+print(df.describe())
+
+# Contar cuántos de cada tipo de sentimiento hay
+print(df['Sentiment'].value_counts())
+
+# Visualización de la distribución de sentimientos
+sns.countplot(x='Sentiment', data=df)
+plt.show()
+
+# Análisis de Product_Type
+print(df['Product_Type'].value_counts())
+sns.countplot(x='Product_Type', data=df)
+plt.show()
+
+# Preprocesamiento de texto
+stop_words = set(stopwords.words('english'))
+
+def preprocess_text(text):
+    word_tokens = word_tokenize(text)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    return " ".join(filtered_sentence)
+
+df['Processed_Description'] = df['Product_Description'].apply(preprocess_text)
+
+print(df.head())
+
+# Agrupar por tipo de producto y sentimiento
+product_sentiment = df.groupby(['Product_Type', 'Sentiment']).size().unstack()
+
+# Gráfico de barras apiladas
+product_sentiment.plot(kind='bar', stacked=True, figsize=(10,7))
+plt.title('Distribución de sentimientos por tipo de producto')
+plt.ylabel('Cantidad de comentarios')
+plt.xlabel('Tipo de producto')
+plt.show()
+
+# Crear una lista con todas las palabras de los comentarios positivos
+positive_comments = df[df['Sentiment'] == 'Positive']['Processed_Description'].str.split().tolist()
+positive_comments = [word for sublist in positive_comments for word in sublist]
+
+# Crear una lista con todas las palabras de los comentarios negativos
+negative_comments = df[df['Sentiment'] == 'Negative']['Processed_Description'].str.split().tolist()
+negative_comments = [word for sublist in negative_comments for word in sublist]
+
+# Contar las palabras más comunes en los comentarios positivos
+positive_counter = Counter(positive_comments)
+print("Palabras más comunes en comentarios positivos:")
+for word, count in positive_counter.most_common(10):
+    print(f"{word}: {count}")
+
+# Contar las palabras más comunes en los comentarios negativos
+negative_counter = Counter(negative_comments)
+print("Palabras más comunes en comentarios negativos:")
+for word, count in negative_counter.most_common(10):
+    print(f"{word}: {count}")
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # IA-e-commerce-NLP-reviews
+
+
+para el prodcut clasification estos son los codigos de la data:
+Attribute Description:
+Text_ID - Unique Identifier
+Product_Description - Description of the product review by a user
+Product_Type - Different types of product (9 unique products)
+Class - Represents various sentiments
+0 - Cannot Say
+1 - Negative
+2 - Positive
+3 - No Sentiment
